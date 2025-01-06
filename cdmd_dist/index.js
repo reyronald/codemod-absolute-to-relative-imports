ecea782aa0977fb6399a3bff0f24a44e9603fc91
Ronald Rey <reyronald@gmail.com>
2025-01-06 11:10:59 -0400

    feat: Jest.mock calls

--- cdmd_dist/index.js
+++ cdmd_dist/index.js
@@ -3,19 +3,45 @@ export default function transform(file, api, options) {
     const j = api.jscodeshift;
     const root = j(file.source);
     let dirtyFlag = false;
+    // Find imports
     root.find(j.ImportDeclaration, {}).forEach((importPath) => {
         const relativeImport = importPath.node.source.value;
         if (typeof relativeImport === "string" &&
             relativeImport.includes("./") &&
             !relativeImport.includes(".svg") &&
             !relativeImport.includes(".less")) {
-            const absoluteImport = path
-                .join(path.dirname(file.path), relativeImport)
-                .replace(process.cwd() + "/", "");
+            const absoluteImport = getAbsoluteImport(file, relativeImport);
             importPath.node.source.value = absoluteImport;
             dirtyFlag = true;
         }
     });
+    // Find jest.mock calls
+    root
+        .find(j.CallExpression, {
+        callee: {
+            type: "MemberExpression",
+            object: { name: "jest" },
+            property: { name: "mock" },
+        },
+    })
+        .forEach((importPath) => {
+        const args = importPath.node.arguments;
+        if (args.length > 0 && j.Literal.check(args[0])) {
+            const relativeImport = args[0].value;
+            if (typeof relativeImport === "string" &&
+                relativeImport.includes("./")) {
+                const absoluteImport = getAbsoluteImport(file, relativeImport);
+                args[0].value = absoluteImport;
+                dirtyFlag = true;
+            }
+        }
+    });
     return dirtyFlag ? root.toSource() : undefined;
+}
+function getAbsoluteImport(file, relativeImport) {
+    const absoluteImport = path
+        .join(path.dirname(file.path), relativeImport)
+        .replace(process.cwd() + "/", "");
+    return absoluteImport;
 }
 export const parser = "tsx";
